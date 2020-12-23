9ea40356d0da6c4ee6f43c73dcb419d3ac4917ce
Abdiel Lopez <a.paperprototype@mail.com>
2020-12-23 16:13:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -156,5 +156,4 @@ def edit():
         return redirect("/search")
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run()
